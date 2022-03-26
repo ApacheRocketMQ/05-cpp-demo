7f31b9f582d373a55573a288c8c44f09b78d8960
tigerweili <tigerweili@tencent.com>
2022-03-26 23:23:20 +0800

    update

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <unistd.h>
 
 #include <condition_variable>
 #include <iomanip>
@@ -9,31 +10,81 @@
 #include <thread>
 #include <DefaultMQProducer.h>
 #include <MQMessage.h>
+#include <MQMessageListener.h>
+#include <DefaultMQPushConsumer.h>
 
 using namespace rocketmq;
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    string namesrv_addr = "127.0.0.1:9876";
-    string topic = "tigerweili_topic_01";
+class RocketMQListener : public MessageListenerConcurrently
+{
+public:
+    RocketMQListener() {}
+    virtual ~RocketMQListener() {}
 
-    if (argc >= 2) {
-        namesrv_addr = argv[1];
+    virtual ConsumeStatus consumeMessage(const vector<MQMessageExt> &msgs)
+    {
+        for (size_t i = 0; i < msgs.size(); ++i)
+        {
+            cout << "msg body: " << msgs[i].getBody() << endl;
+        }
+        return CONSUME_SUCCESS;
     }
+};
+
+int consumer_demo(string namesrv_addr, string topic)
+{
+    RocketMQListener msglistener;
+    string consumer_group_name = "tiger_consumer_group_01";
+    DefaultMQPushConsumer consumer(consumer_group_name);
+
+    consumer.setNamesrvAddr(namesrv_addr);
+    consumer.subscribe(topic, "*");
+    consumer.registerMessageListener(&msglistener);
+
+    try
+    {
+        consumer.start();
+    }
+    catch (MQClientException &e)
+    {
+        cout << e << endl;
+    }
+
+    sleep(100000);
+    return 0;
+}
+
+int producer_demo(string namesrv_addr, string topic)
+{
     string producer_group_name = "tiger_producer_group_01";
-    
+
     DefaultMQProducer producer(producer_group_name);
     producer.setNamesrvAddr(namesrv_addr);
     producer.start();
 
-    MQMessage msg(topic, "*", "Hi RocketMQ"); 
+    MQMessage msg(topic, "*", "Hi RocketMQ");
     SendResult sendResult = producer.send(msg, false);
-    
-    std::cout << namesrv_addr << endl;
 
-    std::cout << sendResult.getMsgId();
+    cout << sendResult.getMsgId();
 
     producer.shutdown();
 
     return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    string namesrv_addr = "127.0.0.1:9876";
+    if (argc >= 2)
+    {
+        namesrv_addr = argv[1];
+    }
+
+    cout << "namesrv地址:" << namesrv_addr << endl;
+    string topic = "tigerweili_topic_01";
+
+    producer_demo(namesrv_addr, topic);
+
+    consumer_demo(namesrv_addr, topic);
 }
